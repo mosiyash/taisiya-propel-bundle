caafcae21fe448577629e828a31e564861ef00f2
Nikita Mosiyash <nikita.mosiyash@gmail.com>
2016-10-12 10:46:03 +0700

    fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-defined(TAISIYA_ROOT) || define('TAISIYA_ROOT', dirname(__DIR__));
+defined('TAISIYA_ROOT') || define('TAISIYA_ROOT', dirname(__DIR__));
 
 return require dirname(__DIR__).'/vendor/autoload.php';
